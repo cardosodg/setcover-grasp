2f196dfcdad6bef95877274e2af64d9e6d75efdf
diego Cardoso <dgiacomellic@gmail.com>
2017-02-17 13:09:46 -0200

    Adicionado calculo de centralidade das instancias.

--- GraphGenerator/shortest_paths.py
+++ GraphGenerator/shortest_paths.py
@@ -33,6 +33,11 @@ for i in xrange(0,len(filesArray)):
     singleSP = inputData.name[:len(inputData.name)-4] + ".ssp"
     allSP = inputData.name[:len(inputData.name)-4] + ".esp"
     
+    betweennessFile = inputData.name[:len(inputData.name)-4] + "_BETWEENNESS.cnt"
+    closenessFile = inputData.name[:len(inputData.name)-4] + "_CLOSENESS.cnt"
+    degreeFile = inputData.name[:len(inputData.name)-4] + "_DEGREE.cnt"
+    loadFile = inputData.name[:len(inputData.name)-4] + "_LOAD.cnt"
+    
     fileOut = open(singleSP, 'w')
     for j in xrange(0,numVert):
 		for k in xrange(0,numVert):
@@ -61,3 +66,31 @@ for i in xrange(0,len(filesArray)):
     fileOut.write(str(numVert) + " " + str(countPath) + "\n")
     fileOut.write(fileOutput)
     fileOut.close()
+
+    fileOut = open(betweennessFile, 'w')
+    cc = nx.betweenness_centrality(G, normalized=False)
+    fileOut.write(str(numVert) + "\n")
+    for(k,v) in cc.items():
+		fileOut.write(str(k) + " " + str(v) + "\n")
+    fileOut.close();
+
+    fileOut = open(closenessFile, 'w')
+    cc = nx.closeness_centrality(G, normalized=False)
+    fileOut.write(str(numVert) + "\n")
+    for(k,v) in cc.items():
+		fileOut.write(str(k) + " " + str(v) + "\n")
+    fileOut.close();
+
+    fileOut = open(degreeFile, 'w')
+    cc = nx.degree_centrality(G)
+    fileOut.write(str(numVert) + "\n")
+    for(k,v) in cc.items():
+		fileOut.write(str(k) + " " + str(v) + "\n")
+    fileOut.close();
+    
+    fileOut = open(loadFile, 'w')
+    cc = nx.load_centrality(G, normalized=False)
+    fileOut.write(str(numVert) + "\n")
+    for(k,v) in cc.items():
+		fileOut.write(str(k) + " " + str(v) + "\n")
+    fileOut.close();
